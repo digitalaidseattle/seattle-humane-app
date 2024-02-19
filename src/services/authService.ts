fd957e888c6d9b347d0667021c4bb505ab0a13cf
Dillon O'Leary <doleary@wisc.edu>
2024-02-19 13:04:25 -0800

    Add and pass test for Azure connection

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -4,37 +4,43 @@
  *  @copyright 2024 Digital Aid Seattle
  *
  */
-import { GetServerSideProps } from 'next'
-import supabaseClient from '../../utils/supabaseClient'
-import { User } from '@supabase/supabase-js'
+import { GetServerSideProps } from "next";
+import supabaseClient from "../../utils/supabaseClient";
+import { User } from "@supabase/supabase-js";
 
 interface AuthProps {
-  initialUser: User
+  initialUser: User;
 }
 class AuthService {
-
   async signOut() {
-    return supabaseClient.auth.signOut()
+    return supabaseClient.auth.signOut();
   }
 
   hasUser() {
-    return supabaseClient.auth.getUser() != null
+    return supabaseClient.auth.getUser() != null;
   }
 
-  getUser = (async () => {
-    return supabaseClient.auth.getUser()
-  })
-
+  getUser = async () => {
+    return supabaseClient.auth.getUser();
+  };
 
   signInWithGoogle = async () => {
-    return supabaseClient.auth.signInWithOAuth({ provider: 'google' })
-      .then(resp => {
-        return resp
-      })
-  }
+    return supabaseClient.auth
+      .signInWithOAuth({ provider: "google" })
+      .then((resp) => {
+        return resp;
+      });
+  };
+
+  signInWithAzure = async () => {
+    return supabaseClient.auth
+      .signInWithOAuth({ provider: "azure" })
+      .then((resp) => {
+        return resp;
+      });
+  };
 }
 
-
-const authService = new AuthService()
-export { authService, AuthService }
-export type { AuthProps }
+const authService = new AuthService();
+export { authService, AuthService };
+export type { AuthProps };
