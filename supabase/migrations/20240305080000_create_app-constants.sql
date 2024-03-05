7b8483bcfe9309ff442abb8b8d886a466aeb21ec
jnakaso <jnakaso@yahoo.com>
2024-03-05 08:23:53 -0800

    fix migration

--- supabase/migrations/20240305080000_create_app-constants.sql
+++ supabase/migrations/20240305080000_create_app-constants.sql
@@ -1,36 +1,14 @@
-create table "public"."Employee" (
-    "id" uuid not null,
-    "created_at" timestamp with time zone not null default now(),
-    "first_name" character varying not null,
-    "last_name" character varying not null,
-    "email" character varying not null,
-    "role" text[] not null
-);
+create table
+  public.app_constants (
+    id uuid not null default gen_random_uuid (),
+    created_at timestamp with time zone not null default now(),
+    label character varying null,
+    value character varying null,
+    type character varying null,
+    constraint app_constants_pkey primary key (id)
+  ) tablespace pg_default;
 
 
-alter table "public"."Employee" enable row level security;
-
-CREATE UNIQUE INDEX "Employee_email_key" ON public."Employee" USING btree (email);
-
-CREATE UNIQUE INDEX "Employee_pkey" ON public."Employee" USING btree (id);
-
-alter table "public"."Employee" add constraint "Employee_pkey" PRIMARY KEY using index "Employee_pkey";
-
-alter table "public"."Employee" add constraint "Employee_email_key" UNIQUE using index "Employee_email_key";
-
-alter table "public"."Employee" add constraint "Employee_id_fkey" FOREIGN KEY (id) REFERENCES auth.users(id) ON DELETE CASCADE not valid;
-
-alter table "public"."Employee" validate constraint "Employee_id_fkey";
-
-grant select, insert, update, delete on table "public"."Employee" to "anon";
-
-grant references, trigger, truncate on table "public"."Employee" to "anon";
-
-grant  select, insert, update, delete on table "public"."Employee" to "authenticated";
-
-grant references, trigger, truncate on table "public"."Employee" to "authenticated";
-
-grant select, insert, update, delete on table "public"."Employee" to "service_role";
-
-grant references, trigger, truncate on table "public"."Employee" to "service_role";
-
+grant select, insert, update, delete on table "public"."APP_CONSTANTS" to "anon";
+grant select, insert, update, delete on table "public"."APP_CONSTANTS" to "authenticated";
+grant select, insert, update, delete on table "public"."APP_CONSTANTS" to "service_role";
