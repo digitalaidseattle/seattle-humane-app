5c2f7d1aa4eee06218f9b53ce30ccf2719bc2305
Dillon O'Leary <doleary@wisc.edu>
2024-04-04 00:16:05 -0700

    Pull client type from generated types

--- src/types.ts
+++ src/types.ts
@@ -1,25 +1,11 @@
 import { Database } from '../supabase/database.types';
 
-// MAIN DATA TYPES
-// Current approach is to keep Domain models and Database models the same. If/when
-// they diverge, we can create separate types for each and use a mapping function
-// ID's are optional as they are not known until the data is persisted in DB
-
 // INCOMPLETE: more fields to be added when confirmed as required
 
-// EXAMPLE of Client type as imported from supabase;
-export type ClientTypeExample = Database['public']['Tables']['clients']['Insert'];
-
-export type ClientType = {
-  id?: BigInteger;
-  first_name: string;
-  last_name: string;
-  email: string;
-  phone: string;
-  postal_code: string;
-  previously_used: string;
-};
+export type ClientType = Database['public']['Tables']['clients']['Insert'];
 export type EditableClientInfo = Omit<ClientType, 'id'>;
+
+export type AnimalTypeExample = Database['public']['Tables']['pets']['Insert'];
 
 // INCOMPLETE: more fields to be added when confirmed as required
 export type AnimalType = {
