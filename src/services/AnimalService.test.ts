2d0af560be4a6195215dee7f6f4c3ba0da3ddfed
Sharrief Shabazz <me@sharrief.com>
2024-04-19 07:21:50 -0700

    Remove the ClientService dependency in AnimalService.test

--- src/services/AnimalService.test.ts
+++ src/services/AnimalService.test.ts
@@ -1,11 +1,9 @@
 import { Animal, AppConstant, Client } from '@types';
 import { AppConstants } from 'src/constants';
 import { AnimalService } from 'src/services/AnimalService';
-import { ClientService } from 'src/services/ClientService';
 import supabaseClient from 'utils/supabaseClient';
-import { v4 as createUUID } from 'uuid';
 
-const testClient: Omit<Client, 'id'> = {
+const testClientInput: Omit<Client, 'id'> = {
   email: 'sh-test@digitalseattle.org',
   first_name: 'John',
   last_name: 'Smith',
@@ -13,41 +11,53 @@ const testClient: Omit<Client, 'id'> = {
   postal_code: '55555',
   previously_used: 'false',
 };
-const testSpecies: Pick<AppConstant, 'type' | 'label' | 'value'> = {
+const testSpeciesInput: Pick<AppConstant, 'type' | 'label' | 'value'> = {
   type: AppConstants.Species,
   label: 'Dog',
   value: 'dog',
 };
-const testAnimal: Omit<Animal, 'id' | 'client_id'> = {
+const testAnimalInput: Omit<Animal, 'id' | 'client_id'> = {
   age: 5,
   name: 'Sparky',
-  species_id: createUUID(),
+  species_id: null, // species must be
   weight: 15,
 };
 // These tests require the database to be running locally
 // Run `npx supabase start` and set the needed vars your .env.test
 // i.e. the URI with the anon_key
 describe('AnimalService', () => {
+  let testClient: Client;
+  beforeAll(async () => {
+    ({ data: testClient } = await supabaseClient.from('clients').insert(testClientInput)
+      .select().maybeSingle());
+  });
   /**
    * Clean up data before tests run, as its possible old test data remains
    * from previous run
    */
   beforeEach(async () => {
-    await supabaseClient.from('clients').delete().eq('email', testClient.email);
+    await supabaseClient.from('pets').delete().eq('name', testAnimalInput.name);
   });
   /**
    * Clean up data after tests run to keep tests independent
    */
   afterEach(async () => {
-    await supabaseClient.from('clients').delete().eq('email', testClient.email);
+    await supabaseClient.from('pets').delete().eq('name', testAnimalInput.name);
+    await supabaseClient.from('app_constants').delete().eq('type', testSpeciesInput.type);
   });
 
+  afterAll(async () => {
+    await supabaseClient.from('pets').delete().eq('name', testAnimalInput.name);
+    await supabaseClient.from('app_constants').delete().eq('type', testSpeciesInput.type);
+  });
   // helpers for commonly used functions
-  const createTestClient = () => ClientService.createClient(testClient);
-  const createTestSpecies = () => AnimalService.createSpecies(testSpecies);
+  const createTestSpecies = () => AnimalService.createSpecies(testSpeciesInput);
 
-  const createTestAnimalForClient = (client_id: string) => AnimalService
-    .createAnimal(testAnimal, client_id);
+  const createTestAnimalForClient = async (client_id: string) => {
+    const species = await createTestSpecies();
+    return AnimalService
+      .createAnimal({ ...testAnimalInput, species_id: species.id }, client_id);
+  };
 
   it('creates new species', async () => {
     const species = await createTestSpecies();
@@ -59,16 +69,14 @@ describe('AnimalService', () => {
     expect(species).toHaveProperty('value', 'dog');
   });
   it('creates new animals', async () => {
-    const client = await createTestClient();
-    const animal = await createTestAnimalForClient(client.id);
+    const animal = await createTestAnimalForClient(testClient.id);
     expect(animal.id).not.toBeFalsy();
   });
   it('finds animals by name, species and client_id', async () => {
-    const client = await createTestClient();
-    const animal = await createTestAnimalForClient(client.id);
-    const foundAnimal = await ClientService.findAnimal(animal, client.id);
+    const animal = await createTestAnimalForClient(testClient.id);
+    const foundAnimal = await AnimalService.findAnimal(animal, testClient.id);
     expect(foundAnimal).toHaveProperty('name', animal.name);
     expect(foundAnimal).toHaveProperty('species_id', animal.species_id);
-    expect(foundAnimal).toHaveProperty('client_id', client.id);
+    expect(foundAnimal).toHaveProperty('client_id', testClient.id);
   });
 });
