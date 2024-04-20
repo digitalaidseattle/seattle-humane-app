6c78f3c6c32ee272f4ba42f2ec4ea5db5f2ede4b
Sharrief Shabazz <me@sharrief.com>
2024-04-20 23:01:09 +0000

    Add sample edge function and test

---
+++ __tests__/edge.test.ts
@@ -0,0 +1,17 @@
+import { createClient } from '@supabase/supabase-js';
+
+async function run() {
+  const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+
+  const { data, error } = await supabase.functions.invoke('hello-world', {
+    body: { name: 'Functions' },
+  });
+  return { data, error };
+}
+
+describe('edge functions', () => {
+  it('can be called', async () => {
+    const res = await run();
+    expect(res.data).not.toBeFalsy();
+  });
+});
