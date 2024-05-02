380043bd194158b2764221f0f604e53c5ae5ec88
Dillon O'Leary <doleary@wisc.edu>
2024-05-02 23:14:15 +0000

    Add species constants to seed data

---
+++ supabase/seed.sql
@@ -0,0 +1,6 @@
+insert into app_constants
+  (label, value, type, active, created_at, changed_at)
+values
+  ('Cat', 'cat', 'species', TRUE, now(), now()),
+  ('Reptile', 'reptile', 'species', TRUE, now(), now()),
+  ('Dog', 'dog', 'species', TRUE, now(), now());
