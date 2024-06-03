a86c378b2b22573bf4c9908a46aa2970ccdb6658
Dillon O'Leary <doleary@wisc.edu>
2024-06-03 00:30:49 +0000

    Add more seed data

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -3,4 +3,8 @@ insert into app_constants
 values
   ('Cat', 'cat', 'species', TRUE, now(), now()),
   ('Reptile', 'reptile', 'species', TRUE, now(), now()),
-  ('Dog', 'dog', 'species', TRUE, now(), now());
+  ('Dog', 'dog', 'species', TRUE, now(), now()),
+  ('Email', 'email', 'source', TRUE, now(), now()),
+  ('Phone Call', 'phone call', 'source', TRUE, now(), now()),
+  ('Surgery', 'surgery', 'category', TRUE, now(), now()),
+  ('Vaccination', 'vaccination', 'category', TRUE, now(), now());
