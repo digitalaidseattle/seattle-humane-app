beba8bbb168db19b8152929dabbd39af6ac4ce4f
Tristan San Juan <tristansj88@gmail.com>
2024-06-09 12:02:06 -0700

    added a temporary hack to refresh the recent tickets list

--- src/hooks/useRecentTickets.ts
+++ src/hooks/useRecentTickets.ts
@@ -1,18 +1,20 @@
 // TODO Remove this sprint Demo hook
 import { ServiceRequestType } from '@types';
+import { useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import ClientService from 'src/services/ClientService';
 
 export default function useRecentTickets() {
   const [tickets, setTickets] = useState<ServiceRequestType[]>([]);
-
+  // Hack:  using url change to trigger to get recent tickets
+  const params = useSearchParams();
   useEffect(() => {
     const getTickets = async () => {
       const data = await ClientService.getRecentTickets();
       setTickets(data);
     };
     getTickets();
-  }, []);
+  }, [params]);
 
   return tickets;
 }
