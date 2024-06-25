680e83cdd9d093beb38aadea4b7c628ff75d1f55
Dillon O'Leary <doleary@wisc.edu>
2024-06-25 23:35:51 +0000

    Add reports to not yet implemented

---
+++ pages/reports/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Reports() {
+  return (
+    <div className="grid flex">
+      <div className="col-12">
+        <div className="card">
+          <h1>Not implemented yet</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Reports;
