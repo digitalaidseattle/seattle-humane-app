ddbd120e4dc8b7f4154178d3d16b90199bccf916
Sharrief Shabazz <me@sharrief.com>
2024-08-17 15:54:15 +0000

    Revert removal of all tickets refresh hack

--- src/hooks/useAllTickets.ts
+++ src/hooks/useAllTickets.ts
@@ -1,16 +1,19 @@
 import { ServiceRequestSummary } from '@types';
+import { useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import * as DataService from '@services/DataService';
 
 export default function useAllTickets() {
   const [tickets, setTickets] = useState<(ServiceRequestSummary)[]>([]);
+  // Hack:  using url change to trigger to get recent tickets
+  const params = useSearchParams();
   useEffect(() => {
     const getTickets = async () => {
       const data = await DataService.getServiceRequestSummary();
       setTickets(data);
     };
     getTickets();
-  }, []);
+  }, [params]);
 
   return tickets;
 }
