2fa64b3d8e9c87e5446d1f155cd7e775768fabb7
Adele Beitvashahi <AdelBeit@users.noreply.github.com>
2024-08-18 21:32:25 -0700

    removed duplicate export useTeamMembers.ts
    export const mockTeamMemberOptions = [mockTeamMember1, mockTeamMember2, mockTeamMember3];
    is also exported from utils/testdata

--- src/hooks/__mocks__/useTeamMembers.ts
+++ src/hooks/__mocks__/useTeamMembers.ts
@@ -1,7 +1,6 @@
 import type { TeamMeberOption } from '@hooks/useTeamMembers';
-import { mockTeamMember1, mockTeamMember2, mockTeamMember3 } from '@utils/TestData';
+import { mockTeamMemberOptions } from '@utils/TestData';
 
-export const mockTeamMemberOptions = [mockTeamMember1, mockTeamMember2, mockTeamMember3];
 export default function useTeamMembers(): TeamMeberOption[] {
   return mockTeamMemberOptions.map((tm) => ({
     value: tm.id,
