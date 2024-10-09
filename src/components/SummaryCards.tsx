b181b6c8a2c7e2db9cb22b27d4642fac108bcc4d
Dillon O'Leary <doleary@wisc.edu>
2024-10-09 23:57:41 +0000

    Fix layout of summary cards

--- src/components/SummaryCards.tsx
+++ src/components/SummaryCards.tsx
@@ -7,15 +7,15 @@ export default function SummaryCards() {
 
   return (
     <div className="grid col-12">
-      <div className="col-12 lg:col-12 xl:col-6">
+      <div className="col-12 lg:col-6 xl:col-3">
         <div className="card mb-0">
           <div className="flex justify-content-between mb-3">
             <div>
               <span className="block text-500 font-medium mb-3">My Newly Assigned Cases</span>
               <div className="text-900 font-medium text-xl">{myTickets.data.length}</div>
             </div>
-            <div className="flex align-items-center justify-content-center bg-orange-100 border-round" style={{ width: '2.5rem', height: '2.5rem' }}>
-              <i className="pi pi-bell text-orange-500 text-xl" />
+            <div className="flex align-items-center justify-content-center bg-teal-100 border-round" style={{ width: '2.5rem', height: '2.5rem' }}>
+              <i className="pi pi-calendar text-teal-500 text-xl" />
             </div>
           </div>
         </div>
