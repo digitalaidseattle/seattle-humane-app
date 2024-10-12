bb08b4754088c81bea4270902a2e5ed6a840f1f0
Adele Beitvashahi <adelbeit@gmail.com>
2024-10-12 17:49:06 +0000

    useFilters test

--- __tests__/src/hooks/useFilters.test.tsx
+++ __tests__/src/hooks/useFilters.test.tsx
@@ -1,35 +1,45 @@
-import useMyTickets from '@hooks/useMyTickets';
-import { UserContext } from '@context/usercontext';
-import { renderHook, waitFor } from '@testing-library/react';
-import { mockServiceRequestSummaries, mockTeamMember1 } from '@utils/TestData';
-import { User } from '@supabase/supabase-js';
+import { filterByCreatedAt, passThroughFilter } from '@hooks/useFilters';
 
-jest.mock('@services/DataService');
+describe('useFilter hook', () => {
+  const dates = ['1/1/2000', '1/2/2000', '3/1/2000', '1/1/2020', '12/12/2010'];
 
-it('returns the signed-in user\'s tickets tickets from the db', async () => {
-  //* Act
-  /** Hook needs the user context to find cases for the user */
-  const wrapper = ({ children }) => (
-    <UserContext.Provider value={{
-      user: { email: mockTeamMember1.email } as User,
-      setUser: () => { },
-    }}
-    >
-      {children}
-    </UserContext.Provider>
-  );
-
-  const { result } = renderHook(useMyTickets, { wrapper });
-
-  //* Assert
-  await waitFor(() => {
-    expect(result.current.isLoading).toBe(false);
+  it('passthrough filter always return true', () => {
+    expect(passThroughFilter()).toBe(true);
   });
-  const mockMyTickets = mockServiceRequestSummaries
-    .filter((t) => t.team_member.email === mockTeamMember1.email);
-  /** Ensure there is actually enough mock test data to filter one user from the rest */
-  expect(mockServiceRequestSummaries.length).toBeGreaterThan(0);
-  expect(mockMyTickets.length).toBeGreaterThan(0);
-  expect(mockMyTickets.length).toBeLessThan(mockServiceRequestSummaries.length);
-  expect(result.current.data).toEqual(mockMyTickets);
+
+  it('exact date filter', () => {
+    const date = '1/1/1999';
+    const filter = { sign: '=', filterDate: date };
+    expect(filterByCreatedAt(date, filter)).toBe(true);
+    expect(filterByCreatedAt('1/1/2020', filter)).toBe(false);
+  });
+
+  it('dates after filter', () => {
+    const expected = ['3/1/2000', '1/1/2020', '12/12/2010'];
+    const filter = { sign: '>', filterDate: '2/20/2000' };
+    const actual = dates.filter(d => filterByCreatedAt(d, filter));
+    expect(actual.length).toBe(3);
+    expect(actual).toEqual(expected);
+  });
+
+  it('dates before filter', () => {
+    const expected = ['1/1/2000', '1/2/2000', '3/1/2000'];
+    const filter = { sign: '<', filterDate: '1/1/2005' };
+    const actual = dates.filter(d => filterByCreatedAt(d, filter));
+    expect(actual.length).toBe(3);
+    expect(actual).toEqual(expected);
+  });
+
+  it('return empty array when no dates match filter', () => {
+    const beforeFilter = { sign: '<', filterDate: '1/1/1000' };
+    const afterFilter = { sign: '>', filterDate: '1/1/3000' };
+    const exactFilter  = { sign: '=', filterDate: '1/1/1000' };
+    const actualBefore = dates.filter(d => filterByCreatedAt(d, beforeFilter));
+    const actualExact = dates.filter(d => filterByCreatedAt(d, exactFilter));
+    const actualAfter = dates.filter(d => filterByCreatedAt(d, afterFilter));
+    const expected = [];
+    expect(actualBefore).toEqual(expected);
+    expect(actualExact).toEqual(expected);
+    expect(actualAfter).toEqual(expected);
+  });
 });
