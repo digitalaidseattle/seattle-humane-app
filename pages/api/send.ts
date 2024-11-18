762eaf03db8d095a62150b629fe18e307fbf8e3c
Adele Beitvashahi <adelbeit@gmail.com>
2024-11-18 01:56:59 +0000

    lint

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -1,36 +1,36 @@
-import ServiceRequestNotification from "@components/email-templates/ServiceRequestNotification";
-import { NextApiRequest, NextApiResponse } from "next";
-import { Resend } from "resend";
+import ServiceRequestNotification from '@components/email-templates/ServiceRequestNotification';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const RESEND_FROM_EMAIL = process.env.RESEND_FROM_EMAIL;
-const RESEND_TO_EMAIL = process.env.RESEND_TO_EMAIL;
+const { RESEND_FROM_EMAIL } = process.env;
+const { RESEND_TO_EMAIL } = process.env;
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-   if (req.method !== 'POST'){
-      return res.status(405).json({erorr:"Method not allowed. use POST."});
-   }
-   try {
-      req.body = { service_category: 'vaccination', urgent: true, created_at: '10/10/2024' };
-      const { service_category, urgent, created_at } = req.body;
-      if (!service_category || urgent === undefined || !created_at) {
-         return res.status(400).json({ error: "Missing required fields: service_category, urgent, or created_at." });
-      }
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  // if (req.method !== 'POST'){
+  //    return res.status(405).json({erorr:"Method not allowed. use POST."});
+  // }
+  try {
+    req.body = { category: 'vaccination', urgent: true, createdAt: '10/10/2024' };
+    const { category, urgent, createdAt } = req.body;
+    if (!category || urgent === undefined || !createdAt) {
+      return res.status(400).json({ error: 'Missing required fields: service_category, urgent, or created_at.' });
+    }
 
-      const { data, error } = await resend.emails.send({
-         from: RESEND_FROM_EMAIL,
-         to: RESEND_TO_EMAIL,
-         subject: `New ${urgent && "[Urgent]"} Service Request Notification`,
-         react: ServiceRequestNotification({ service_category, urgent, created_at }),
-      });
+    const { data, error } = await resend.emails.send({
+      from: RESEND_FROM_EMAIL,
+      to: RESEND_TO_EMAIL,
+      subject: `New ${urgent && '[Urgent]'} Service Request Notification`,
+      react: ServiceRequestNotification({ category, urgent, createdAt }),
+    });
 
-      if (error) {
-         return res.status(400).json(error);
-      }
+    if (error) {
+      return res.status(400).json(error);
+    }
 
-      return res.status(200).json(data);
-   } catch (err) {
-      console.error("Error processing request:", err);
-      return res.status(500).json({ error: "Internal server error." });
-   }
+    return res.status(200).json(data);
+  } catch (err) {
+    console.error('Error processing request:', err);
+    return res.status(500).json({ error: 'Internal server error.' });
+  }
 }
--- src/components/email-templates/ServiceRequestNotification.tsx
+++ src/components/email-templates/ServiceRequestNotification.tsx
@@ -1,13 +1,30 @@
-import { ServiceRequestSummary } from "@types";
+import { ServiceRequestSummary } from '@types';
 
-type EmailTemplateProps = Pick<ServiceRequestSummary, 'service_category' | 'urgent' | 'created_at'>;
+interface EmailTemplateProps {
+  category: ServiceRequestSummary['service_category'],
+  urgent: ServiceRequestSummary['urgent'],
+  createdAt: ServiceRequestSummary['created_at']
+}
 
-export default function ServiceRequestNotification(props: EmailTemplateProps) {
-   return (
-      <div>
-         <h3>Heya! There's a new {props.urgent && "URGENT"} client service request waiting for you in the dashboard!</h3>
-         <h4>Service Category: {props.service_category}</h4>
-         <h4>Created on: {props.created_at}</h4>
-      </div>
-   );
+export default function ServiceRequestNotification(
+  { category, urgent, createdAt }: EmailTemplateProps,
+) {
+  return (
+    <div>
+      <h3>
+        Heya! There&apos;s a new
+        {urgent && 'URGENT'}
+        {' '}
+        client service request waiting for you in the dashboard!
+      </h3>
+      <h4>
+        Service Category:
+        {category}
+      </h4>
+      <h4>
+        Created on:
+        {createdAt}
+      </h4>
+    </div>
+  );
 }
