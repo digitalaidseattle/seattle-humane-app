d632debb1e2a2a8f1c684ccc6bb92873ae165437
Dillon O'Leary <doleary@wisc.edu>
2024-12-14 00:15:31 +0000

    I don't know if this is the right fix but without it there was a lot of jitter

--- src/hooks/useAllTickets.ts
+++ src/hooks/useAllTickets.ts
@@ -1,19 +1,18 @@
-import { useMemo } from 'react';
 import * as DataService from '@services/DataService';
 import useSWR from 'swr';
 
 export default function useAllTickets() {
   const {
-    data: fetchedData, isLoading, isValidating,
+    data, isLoading, isValidating,
   } = useSWR(
     'dataservice/alltickets',
     async () => DataService.getServiceRequestSummary(),
     { refreshInterval: 10000 },
   );
 
-  const data = useMemo(() => fetchedData ?? [], [fetchedData]);
-
   return {
-    data, isLoading, isValidating,
+    data,
+    isLoading: !data && isLoading,
+    isValidating: !data && isValidating,
   };
 }
